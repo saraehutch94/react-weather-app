68a7f1f72663f46897c0fba79a27967274e48d95
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2021-12-21 15:05:01 -0500

    map through data array, return WeatherForecast component with props

--- src/App.js
+++ src/App.js
@@ -2,8 +2,18 @@ import "./App.css";
 import weatherArray from "./weatherData.js";
 import WeatherForecast from "./components/WeatherForecast";
 
+const newWeatherArray = weatherArray.map((object, index) => {
+  return (
+    <WeatherForecast
+      key={index}
+      img={object.img}
+      conditions={object.conditions}
+      time={object.time}
+    />
+  );
+});
+
 function App() {
-  console.log(weatherArray);
   return "hello";
 }
 
