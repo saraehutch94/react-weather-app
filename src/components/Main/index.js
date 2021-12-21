29b862602b1434cc395647f67183d82cb8ccdbdd
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2021-12-21 16:46:38 -0500

    refactor code to add weatherComponents in main component

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,5 +1,11 @@
+import "./Main.css";
+
 function Main(props) {
-  return <div>{props.weatherComponents}</div>;
+  return (
+    <main class="main-tag">
+      <div class="main-div">{props.weatherComponents}</div>
+    </main>
+  );
 }
 
 export default Main;
