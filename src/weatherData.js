a543fcc53c3f85d4932fbb68636753047f3382fe
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2021-12-21 14:28:09 -0500

    add data array with objects for app

---
+++ src/weatherData.js
@@ -0,0 +1,27 @@
+export default [
+  {
+    img: "http://res.cloudinary.com/jkeohan/image/upload/v1535732381/day.svg",
+    conditions: "Sunny",
+    time: "Day",
+  },
+  {
+    img: "http://res.cloudinary.com/jkeohan/image/upload/v1535732381/night.svg",
+    conditions: "Clear",
+    time: "Day",
+  },
+  {
+    img: "http://res.cloudinary.com/jkeohan/image/upload/v1535732381/stormy.svg",
+    conditions: "Clear",
+    time: "Day",
+  },
+  {
+    img: "http://res.cloudinary.com/jkeohan/image/upload/v1535732381/cloudy-day_t7ckxp.svg",
+    conditions: "Partly Sunny",
+    time: "Night",
+  },
+  {
+    img: "http://res.cloudinary.com/jkeohan/image/upload/v1535732381/cloudy-night.svg",
+    conditions: "Clear",
+    time: "Day",
+  },
+];
